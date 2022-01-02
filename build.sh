1fe54796ba24a9a4972fe2fc95a8ae0f4e967d43
cruizba <carlos.ruizbal@gmail.com>
2022-01-02 19:32:29 +0100

    Make all CI registry parameters configurable

--- build.sh
+++ build.sh
@@ -6,20 +6,21 @@ if [[ "${FROM_CI}" == "true" ]]; then
     export DOCKERHUB_USERNAME="${DOCKERHUB_USERNAME:-}"
     export DOCKERHUB_PASSWORD="${DOCKERHUB_PASSWORD:-}"
     export DOCKER_CLI_EXPERIMENTAL=enabled
-    export GITHUB_TOKEN="${GITHUB_TOKEN}"
+    export GITHUB_TOKEN="${GITHUB_TOKEN:-}"
+    export GITHUB_USERNAME="${GITHUB_USERNAME:-}"
     docker buildx create --name builder-multi
     docker buildx use builder-multi
     if [[ -n ${DOCKERHUB_USERNAME} ]] && [[ ${DOCKERHUB_PASSWORD} ]]; then
-        echo "${DOCKERHUB_PASSWORD}" | docker login --username cruizba --password-stdin
-        docker buildx build --platform linux/arm,linux/arm64,linux/amd64 --push -t cruizba/samba:latest .
+        echo "${DOCKERHUB_PASSWORD}" | docker login --username "${DOCKERHUB_USERNAME}" --password-stdin
+        docker buildx build --platform linux/arm,linux/arm64,linux/amd64 --push -t "${DOCKERHUB_USERNAME}"/samba:latest .
         docker logout
     fi
     if [[ -n "${GITHUB_TOKEN}" ]]; then
-        echo "${GITHUB_TOKEN}" | docker login ghcr.io --username cruizba --password-stdin
-        docker buildx build --platform linux/arm,linux/arm64,linux/amd64 --push -t ghcr.io/cruizba/samba:latest .
+        echo "${GITHUB_TOKEN}" | docker login ghcr.io --username "${GITHUB_USERNAME}" --password-stdin
+        docker buildx build --platform linux/arm,linux/arm64,linux/amd64 --push -t ghcr.io/"${GITHUB_USERNAME}"/samba:latest .
         docker logout
     fi    
 else
-    docker build -t cruizba/samba:latest .
+    docker build -t samba:latest .
 fi
 
