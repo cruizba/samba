548bf5d8876f25b49fd950e3ec089543e7d21cd1
cruizba <carlos.ruizbal@gmail.com>
2022-01-02 20:16:49 +0100

    Add more platforms

--- build.sh
+++ build.sh
@@ -12,12 +12,12 @@ if [[ "${FROM_CI}" == "true" ]]; then
     docker buildx use builder-multi
     if [[ -n ${DOCKERHUB_USERNAME} ]] && [[ ${DOCKERHUB_PASSWORD} ]]; then
         echo "${DOCKERHUB_PASSWORD}" | docker login --username "${DOCKERHUB_USERNAME}" --password-stdin
-        docker buildx build --platform linux/arm,linux/arm64,linux/amd64 --push -t "${DOCKERHUB_USERNAME}"/samba:latest .
+        docker buildx build --platform linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64/v8 --push -t "${DOCKERHUB_USERNAME}"/samba:latest .
         docker logout
     fi
     if [[ -n "${GITHUB_TOKEN}" ]]; then
         echo "${GITHUB_TOKEN}" | docker login ghcr.io --username "${GITHUB_USERNAME}" --password-stdin
-        docker buildx build --platform linux/arm,linux/arm64,linux/amd64 --push -t ghcr.io/"${GITHUB_USERNAME}"/samba:latest .
+        docker buildx build --platform linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64/v8 --push -t ghcr.io/"${GITHUB_USERNAME}"/samba:latest .
         docker logout
     fi    
 else
